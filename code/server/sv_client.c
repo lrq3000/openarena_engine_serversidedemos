8b6b82ff67d73c63076075c60ddc3f94c749835a
Stephen L <lrq3000@gmail.com>
2012-02-28 18:55:19 +0100

    v0.6.7 auto spectator and team change management fixed - stable
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- code/server/sv_client.c
+++ code/server/sv_client.c
@@ -1509,7 +1509,9 @@ void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK ) {
 		if (!u->name && sv.state == SS_GAME && (cl->state == CS_ACTIVE || cl->state == CS_PRIMED || cl->demoClient )) {
 			if ( sv.demoState == DS_RECORDING ) { // if demo is recording, we store this command and clientid
 				SV_DemoWriteClientCommand( cl, s );
-			} else if ( sv.demoState == DS_PLAYBACK && ( !strcmp(Cmd_Argv(0), "team") && strcmp(Cmd_Argv(1), "s") && strcmp(Cmd_Argv(1), "spectator") ) ) { // if there is a demo playback, we prevent any team change and issue a chat messsage (except if the player join team spectator again)
+			} else if ( sv.demoState == DS_PLAYBACK &&
+				   ( (cl - svs.clients) >= sv_democlients->integer ) && ( (cl - svs.clients) < sv_maxclients->integer ) && // preventing only real clients commands (not democlients commands replayed)
+				   ( !strcmp(Cmd_Argv(0), "team") && strcmp(Cmd_Argv(1), "s") && strcmp(Cmd_Argv(1), "spectator") ) ) { // if there is a demo playback, we prevent any real client from doing a team change, if so, we issue a chat messsage (except if the player join team spectator again)
 				SV_SendServerCommand(cl, "chat \"^3Can't join a team when a demo is replaying!\""); // issue a chat message only to the player trying to join a team
 				return;
 			}
